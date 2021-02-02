43d87a18de4753223bbe7161a95e0463eb73749c
Alexander Trakhimenok <alex@trakhimenok.com>
2021-02-02 15:01:43 +0000

    feature(models): dataset def

--- packages/models/datasets.go
+++ packages/models/datasets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/qri-io/jsonschema"
 	"github.com/strongo/validation"
 )
@@ -9,8 +10,32 @@ import (
 // DatasetDefinition describes dataset
 type DatasetDefinition struct {
 	ProjectEntity
-	Type       string `json:"type"`
-	JSONSchema string `json:"jsonSchema"`
+	Type       string             `json:"type"` // Supported types: "recordset", "json"
+	JSONSchema string             `json:"jsonSchema,omitempty"`
+	Fields     RecordsetFieldDefs `json:"fields,omitempty"`
+}
+
+type RecordsetFieldDefs []RecordsetFieldDef
+
+// Validate returns error if not valid
+func (v RecordsetFieldDefs) Validate() error {
+	for i, field := range v {
+		if err := field.Validate(); err != nil {
+			return fmt.Errorf("invalid field at index=%v, name=%v: %w", i, field.Name, err)
+		}
+	}
+	return nil
+}
+
+type RecordsetFieldDef struct {
+	Name     string          `json:"name"`
+	Type     string          `json:"type"`
+	Required bool            `json:"required"`
+	Meta     *EntityFieldRef `json:"meta,omitempty"`
+}
+
+func (v RecordsetFieldDef) Validate() error {
+	return nil
 }
 
 // Validate returns error if not valid
@@ -33,6 +58,9 @@ func (v DatasetDefinition) Validate() error {
 		return validation.NewErrRecordIsMissingRequiredField("type")
 	default:
 		return validation.NewErrBadRecordFieldValue("type", "unknown value: "+v.Type)
+	}
+	if err := v.Fields.Validate(); err != nil {
+		return validation.NewErrBadRequestFieldValue("fields", err.Error())
 	}
 	return nil
 }
