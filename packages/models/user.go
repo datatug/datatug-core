e101ddf3c0c9a34c1df8543eae71fc01079f6846
Alexander Trakhimenok <alex@trakhimenok.com>
2021-06-13 10:54:56 +0100

    use map for projects in user record

--- packages/models/user.go
+++ packages/models/user.go
@@ -4,23 +4,16 @@ import "fmt"
 
 // UserDatatugInfo holds user info for DataTug project
 type UserDatatugInfo struct {
-	Projects []ProjectBrief `json:"projects,omitempty" firestore:"projects,omitempty"`
+	Projects map[string]ProjectBrief `json:"projects,omitempty" firestore:"projects,omitempty"`
 }
 
 // Validate returns error if not valid
 func (v UserDatatugInfo) Validate() error {
 	if len(v.Projects) > 0 {
-		ids := make([]string, 0, len(v.Projects))
 		for i, p := range v.Projects {
 			if err := p.Validate(true); err != nil {
 				return fmt.Errorf("invalid project at index %v: %w", i, err)
 			}
-			for j, id := range ids {
-				if id == p.ID {
-					return fmt.Errorf("duplicate project ID at indexes %v & %v: %v", j, i, id)
-				}
-			}
-			ids = append(ids, p.ID)
 		}
 	}
 	return nil
