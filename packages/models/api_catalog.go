669173d6b42716687fa0b53c463cfe33f01ef8b5
Alexander Trakhimenok <alex@trakhimenok.com>
2021-07-12 20:13:59 +0100

    api catalog models validators

--- packages/models/api_catalog.go
+++ packages/models/api_catalog.go
@@ -4,17 +4,31 @@ type ApiService struct {
 	ProjectItem
 }
 
+func (v ApiService) Validate() error {
+	if err := v.ProjectItem.Validate(true); err != nil {
+		return err
+	}
+	return nil
+}
+
 type ApiEndpoint struct {
 	ProjectItem
-	QueryType    string   `json:"queryType" firestore:"queryType"`       // http, GraphQL
-	ContentTypes []string `json:"contentTypes" firestore:"contentTypes"` // application/json, text
-	Method       string   `json:"method" firestore:"method"`
-	Schema       string   `json:"schema" firestore:"schema"`
-	UrlHost      string   `json:"urlHost" firestore:"urlHost"`
-	UrlPath      string   `json:"urlPath" firestore:"urlPath"`
+
+	QueryType string `json:"queryType" firestore:"queryType"` // REST, RPC, GraphQL
+	Method    string `json:"method" firestore:"method"`
+	UrlSchema string `json:"urlSchema" firestore:"urlSchema"`
+	UrlHost   string `json:"urlHost" firestore:"urlHost"`
+	UrlPath   string `json:"urlPath" firestore:"urlPath"`
 
 	FavoritesCount int `json:"favoritesCount" firestore:"favoritesCount"`
 	VotesScore     int `json:"votesScore" firestore:"votesScore"`
 	VotesUp        int `json:"votesUp" firestore:"votesUp"`
 	VotesDown      int `json:"votesDown" firestore:"votesDown"`
+}
+
+func (v ApiEndpoint) Validate() error {
+	if err := v.ProjectItem.Validate(false); err != nil {
+		return err
+	}
+	return nil
 }
