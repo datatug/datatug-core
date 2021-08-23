5bc1132d948f779404986a2dea16d4a0ca0d0261
Alexander Trakhimenok <alex@trakhimenok.com>
2021-08-23 15:33:17 +0300

    inflight coding

--- packages/schemer/mssql/tables.go
+++ packages/schemer/mssql/tables.go
@@ -13,16 +13,36 @@ var _ schemer.TablesProvider = (*tablesProvider)(nil)
 type tablesProvider struct {
 }
 
-func (v tablesProvider) GetTables(_ context.Context, db *sql.DB, catalog, schema string) (schemer.TablesReader, error) {
-	rows, err := db.Query(objectsSQL)
-	if err != nil {
-		return nil, fmt.Errorf("failed to query MSSQL objects: %w", err)
+func (v tablesProvider) GetTables(
+	_ context.Context,
+	db *sql.DB,
+	catalog, schema string,
+) (
+	reader schemer.TablesReader,
+	err error,
+) {
+	if db == nil {
+		panic("required parameter db *sql.DB is nil")
 	}
-	return tablesReader{catalog: catalog, rows: rows}, nil
+	var rows *sql.Rows
+	if schema == "" {
+		rows, err = db.Query(allTablesSQL)
+	} else {
+		rows, err = db.Query(tablesForSchemaSQL, schema)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to query MSSQL tables: %w", err)
+	}
+	reader = tablesReader{
+		catalog: catalog,
+		schema:  schema,
+		rows:    rows,
+	}
+	return reader, nil
 }
 
 //goland:noinspection SqlNoDataSourceInspection
-const objectsSQL = `
+const allTablesSQL = `
 SELECT
        TABLE_SCHEMA,
        TABLE_NAME,
@@ -30,10 +50,19 @@ SELECT
 FROM INFORMATION_SCHEMA.TABLES
 ORDER BY TABLE_SCHEMA, TABLE_NAME`
 
+const tablesForSchemaSQL = `
+SELECT
+       TABLE_NAME,
+       TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = ?
+ORDER BY TABLE_NAME`
+
 var _ schemer.TablesReader = (*tablesReader)(nil)
 
 type tablesReader struct {
 	catalog string
+	schema  string
 	rows    *sql.Rows
 }
 
@@ -46,7 +75,14 @@ func (s tablesReader) NextTable() (*models.Table, error) {
 		return nil, err
 	}
 	var table models.Table
-	if err := s.rows.Scan(&table.Schema, &table.Name, &table.DbType); err != nil {
+	var err error
+	if s.schema == "" {
+		err = s.rows.Scan(&table.Schema, &table.Name, &table.DbType)
+	} else {
+		err = s.rows.Scan(&table.Name, &table.DbType)
+		table.Schema = s.schema
+	}
+	if err != nil {
 		return nil, fmt.Errorf("failed to scan table row into Table struct: %w", err)
 	}
 	table.Catalog = s.catalog
